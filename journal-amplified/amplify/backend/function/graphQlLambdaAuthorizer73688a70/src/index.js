27fd6a17cbbbda585a42cbb5b894aa4fa2658ae9
Tonnyb <tonnyb@joingladys.com>
2023-06-06 15:41:00 +0300

    updates dynamoDb config on send email lambda function

--- journal-amplified/amplify/backend/function/graphQlLambdaAuthorizer73688a70/src/index.js
+++ journal-amplified/amplify/backend/function/graphQlLambdaAuthorizer73688a70/src/index.js
@@ -5,8 +5,12 @@
  */
 
 const AWS = require('aws-sdk');
-const ses = new AWS.SES(); // Create SES service object
-const dynamoDBClient = new AWS.DynamoDB.DocumentClient();
+const ses = new AWS.SES({ region: "us-east-1" }); // Create SES service object
+const dynamoDBClient = new AWS.DynamoDB.DocumentClient({
+  region: 'us-east-1',
+  accessKeyId: process.env.accessKeyId,
+  secretAccessKey: process.env.secretAccessKey
+});
 const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
 
 exports.handler = async (event) => {
@@ -15,22 +19,51 @@ exports.handler = async (event) => {
   const yesterday = new Date(today);
   yesterday.setDate(yesterday.getDate() - 1); // get date 24 hours ago
   
-  console.log('reached here 1')
   const params = {
-    TableName: 'journal',
-    ExpressionAttributeValues: {
-      ':start_date': yesterday.toISOString(),
-      ':end_date': today.toISOString()
-    },
-    FilterExpression: 'createdAt between :start_date and :end_date'
+    TableName: 'Journal-orqzzs3sivgzfbq6t2n3pdzxtm-dev',
+    Key: {
+      userJournalsId: 'df9aef2b-37cb-46b4-ba60-5c3c60b264ce'
+    }
   };
   console.log('reached here 2')
+
+  async function sendJournalEntry(recipientEmail, subject, message) {
+    const AWS = require('aws-sdk');
+    AWS.config.update({ region: 'us-west-2' }); // replace with your preferred AWS region
+  
+    const ses = new AWS.SES();
+  
+    try {
+      await ses.sendEmail({
+        Source: 'betttonny966@gmail.com',
+        Destination: {
+          ToAddresses: [recipientEmail]
+        },
+        Message: {
+          Subject: { Data: subject },
+          Body: { Text: { Data: message } }
+        }
+      }).promise();
+    
+      console.log(`Sent email to user ${recipientEmail} with subject "${subject}"`);
+    } catch (err) {
+      console.error('Failed to send email:', err);
+    }
+  }
+
   try {
       
     // Query DynamoDB for journal entries in the past 24 hours
-    const data = await dynamoDBClient.scan(params).promise();
-    console.log('data 1',data)
-    const entries = data.Items;
+    let entries;
+
+    try {
+      const data = await dynamoDBClient.scan(params).promise();
+
+      entries= data.Items;
+
+    } catch (err) {
+      console.log('err1', err);
+    }
     
     // Get list of users who posted an entry in the past 24 hours
     let users = entries.reduce((result, entry) => {
@@ -39,6 +72,8 @@ exports.handler = async (event) => {
       }
       return result;
     }, []);
+
+    console.log('reached here 3', users)
     
     // Shuffle the array of user IDs to randomize the order
     for (let i = users.length - 1; i > 0; i--) {
@@ -60,27 +95,22 @@ exports.handler = async (event) => {
       
       // Retrieve user email from User Pool using sub value
       const recipientUser = await cognitoIdentityServiceProvider.adminGetUser({
-        UserPoolId: 'us-east-1_boIhlFLhG', // Replace with your User Pool ID
+        UserPoolId: 'us-east-1_OpEmXTxRY', // Replace with your User Pool ID
         Username: recipientSub
       }).promise();
+
+      console.log('reached here 4', recipientUser)
       
       const recipientEmail = recipientUser.UserAttributes.find(attr => attr.Name === 'email').Value;
+
+      console.log('reached here 5', recipientEmail)
       
       const message = `Hello,\n\nHere's a journal entry submitted by another user:\n\nTitle: ${randomEntry.title}\nContent: ${randomEntry.content}`;
-      
-      await ses.sendEmail({
-        Source: 'sender@example.com',
-        Destination: {
-          ToAddresses: [recipientEmail]
-        },
-        Message: {
-          Subject: { Data: 'Daily Journal Entry' },
-          Body: { Text: { Data: message } }
-        }
-      }).promise();
-      
-      console.log(`Sent email containing journal entry from user ${randomEntry.userJournalsId} to user ${recipientEmail}`);
+
+      console.log('reached here 6', message)
     }
+
+    sendJournalEntry(recipientEmail, 'Your Daily Journal Entry', message);
     
     return 'Successfully sent emails';
     
